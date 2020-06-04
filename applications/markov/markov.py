73caf95a58ac2b5c3e672a6140d4c22663f26d80
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-04 08:59:53 -0500

    add comments

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -10,23 +10,29 @@ words = words.split()
 
 
 def build_cache(lst):
+    ''' take in a list and build a dict cache of all possible 
+    succeeding words '''
     cache = {}
     for cur in range(len(lst)-1):
-        if lst[cur] in cache:
+        if lst[cur] in cache:  # if the word is alread in the cache
+            # add following word to its value list
             cache[lst[cur]].append(lst[cur+1])
-        else:
+        else:  # add the word as a key and the following word as a value list
             cache[lst[cur]] = [lst[cur+1]]
 
     return cache
 
 
 def build_corpus(cache, start):
+    ''' take in a cache of words and a start word and build a sentence until stop 
+    condition is met '''
     sequence = [start]
     current = start
-    while current in cache:
+    while current in cache:  # make sure the current word is a key in the cache
+        # stop if the current word meets the stop word conditions
         if current[-1] in list('.?!') or current[-2:] in ['."', '?"', '!"']:
             break
-        else:
+        else:  # add the word to the sequence list and make it the next word
             sequence.append(random.choice(cache[current]))
             current = sequence[-1]
     return " ".join(sequence)
