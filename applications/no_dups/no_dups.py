f9a28091f28b353cf788301c866a1bbf9feb8193
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-03 19:09:13 -0500

    no dupes done

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,9 @@
 def no_dups(s):
     # Your code here
-
+    s = s.split()
+    seen = set()
+    words = [x for x in s if not (x in seen or seen.add(x))]
+    return ' '.join(words)
 
 
 if __name__ == "__main__":
