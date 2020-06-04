e4a36dccce4f3800d853d4e151981fa62a1b2fb9
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-04 09:16:31 -0500

    add comments

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -2,18 +2,22 @@ import string
 
 
 def word_count(s):
-    punc = 0
-    counts = {}
+    # punc = 0
     # Your code here
-    ignore = list('":;,.-+=/\\|[]{}()*^&')
+    # ignore = list('":;,.-+=/\\|[]{}()*^&')
     # If the input contains no ignored characters, return an empty dictionary.
     # for i in ignore:
     #     if i in s:
     #         punc += 1
     # if punc == 0:
     #     return {}
+
+    # dict cache for word counts
+    counts = {}
+    # remove unwanted punctuation and convert to a list
     s = s.lower().translate(str.maketrans("", "", '":;,.-+=/\\|[]{}()*^&'))
     list_s = s.split()
+    # either increase the count if the word is found or add the word to cache
     for word in list_s:
         if word in counts:
             counts[word] += 1
