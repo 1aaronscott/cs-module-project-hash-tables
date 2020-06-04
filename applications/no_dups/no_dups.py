5c2a681057cfcf1abd07fc3e91ac3b66603f5727
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-04 09:10:45 -0500

    add comments

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,12 @@
 def no_dups(s):
     # Your code here
     s = s.split()
+    # make a set containing unique values
     seen = set()
+    # Use a set to keep track of which elements you’ve seen
+    # At the same time, populate the new list by a list comprehension
+    # relies on the fact that set.add() returns None
+    # http://www.martinbroadhurst.com/removing-duplicates-from-a-list-while-preserving-order-in-python.html
     words = [x for x in s if not (x in seen or seen.add(x))]
     return ' '.join(words)
 
