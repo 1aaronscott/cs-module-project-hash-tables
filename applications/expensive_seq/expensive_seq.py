5f899928690e7294554157ead21447818b1e140d
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-04 07:59:36 -0500

    add comments

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -5,9 +5,9 @@ cache = {}
 def expensive_seq(x, y, z):
     # Your code here
     key = (x, y, z)
-    if x <= 0:
+    if x <= 0:  # handle base case
         return y+z
-    else:
+    else:  # recurse if key not already present
         if key not in cache:
             cache[key] = expensive_seq(
                 x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
