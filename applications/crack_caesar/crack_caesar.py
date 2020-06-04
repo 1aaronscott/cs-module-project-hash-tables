f4ac1361e19e611a3c68ba8c8e279b521faf2323
1aaronscott <1aaronscott@users.noreply.github.com>
2020-06-03 23:06:08 -0500

    caesar done

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,28 @@
 
 # Your code here
 
+most_common = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+               'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+with open("ciphertext.txt") as f:
+    words = f.read()
+
+
+def letters_by_count(corpus):
+    counts = {}
+    corpus = corpus.translate(str.maketrans(
+        "", "", ' ":;,.-+=/\\|[]{}()*^&\n\'\!—?1'))
+
+    for c in corpus:
+        if c in counts:
+            counts[c] += 1
+        else:
+            counts[c] = 1
+
+    items = list(counts.items())
+    items.sort(key=lambda e: e[1], reverse=True)
+    items = [x[0] for x in items]
+    return items
+
+
+converter = dict(zip(letters_by_count(words), most_common))
+print(words.translate(str.maketrans(converter)))
